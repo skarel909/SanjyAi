23d40765d4d226f683ab8bb94743da3a30f28984
skarel909 <kotoltopup@gmail.com>
2026-05-27 17:45:50 +0700

    Update AuthContext.tsx

--- AuthContext.tsx
+++ AuthContext.tsx
@@ -1,5 +1,5 @@
 import React, { createContext, useContext, useEffect, useState } from 'react';
-import { supabase } from '../lib/supabase';
+import { supabase } from './supabase';
 import { User, AuthContextType } from '../types';
 
 const AuthContext = createContext<AuthContextType | undefined>(undefined);
